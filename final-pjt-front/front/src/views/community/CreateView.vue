d0ea21c9004a4a3147157425bfe7b7fbb734d068
조수현 <jeniffer0812@naver.com>
2023-11-19 16:22:05 +0900

    .

--- final-pjt-front/front/src/views/community/CreateView.vue
+++ final-pjt-front/front/src/views/community/CreateView.vue
@@ -1,71 +1,47 @@
 <template>
-  <!-- 게시글 생성 -->
   <div>
-    <h2>게시글 작성</h2>
+    <h1>게시글 생성 페이지</h1>
     <form @submit.prevent="createArticle">
-      <label for="title">제목 : </label>
-      <input type="text" id="title" v-model.trim="title" /><br />
-      <label for="content">내용 : </label>
-      <textarea id="content" cols="30" rows="10" v-model="content"></textarea
-      ><br />
-      <label for="category">카테고리</label>
-      <select id="category" v-model="category">
-        <option value="예금">예금</option>
-        <option value="적금">적금</option>
-        <option value="기타">기타</option></select
-      ><br />
-      <input type="submit" id="submit" />
+      <input type="text" v-model="title" placeholder="제목을 입력하세요" />
+      <input type="text" v-model="content" placeholder="내용을 입력하세요" />
+      <button type="submit">생성</button>
     </form>
   </div>
 </template>
 
-<script>
+<script setup>
 import axios from "axios";
-const API_URL = "http://127.0.0.1:8000";
-const user_id = 2;
+import {ref} from "vue";
+import {useCounterStore} from "@/stores/counter";
+import {useAccountStore} from "@/stores/account";
 
-export default {
-  name: "CreateView",
-  data() {
-    return {
-      title: null,
-      content: null,
-      category: null,
-    };
-  },
-  methods: {
-    createArticle() {
-      const title = this.title;
-      const content = this.content;
-      const category = this.category;
-      const user_id = this.user_id;
+const store = useCounterStore();
+const store2 = useAccountStore();
+const title = ref("");
+const content = ref("");
 
-      if (!title) {
-        alert("제목을 입력해주세요");
-        return;
-      } else if (!content) {
-        alert("내용을 입력해주세요");
-        return;
-      } else if (!category) {
-        alert("카테고리를 지정해주세요");
-      }
-      axios({
-        method: "POST",
-        url: `${API_URL}/api/v1/articles/`,
-        data: { title, content, category, user_id },
-        // headers: {
-        //   Authorization: `Token ${this.$store.state.token}`
-        // }
+const createArticle = () => {
+  axios({
+        method: "post",
+        url: `${store.API_URL}/api/v1/articles/`,
+        data: {
+          title: title.value,
+          content: content.value,
+        },
+        headers: {
+          Authorization: `Token ${store2.token}`,
+      },
       })
-        .then((res) => {
-          this.$router.push({ name: "ArticleView" });
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    },
-  },
+    .then((res) => {
+      console.log(res.data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
+
 </script>
 
-<style></style>
+<style scoped>
+
+</style>
