51403b7abcab69bb6514bf28188bbfa82da60188
dreamingbeom <128280944+dreamingbeom@users.noreply.github.com>
2023-11-21 23:54:26 +0900

    기간별 정렬 진행중

--- final-pjt-front/front/src/components/deposit/SavingProductItemBasic.vue
+++ final-pjt-front/front/src/components/deposit/SavingProductItemBasic.vue
@@ -1,23 +1,24 @@
 <template>
-  <div v-if="item">
-    <v-avatar class="icon" image="https://item.kakaocdn.net/do/9fc0462374fa73111ee6b47046b9ce7b8b566dca82634c93f811198148a26065" size="40"></v-avatar>
+  <div>
+    <v-avatar
+      class="icon"
+      image="https://item.kakaocdn.net/do/9fc0462374fa73111ee6b47046b9ce7b8b566dca82634c93f811198148a26065"
+      size="40"
+    ></v-avatar>
     {{ savingProduct.kor_co_nm }}
     {{ savingProduct.fin_prdt_nm }}
     <h3>기본 {{ maxRate1 }} %</h3>
     <p>최고 {{ maxRate2 }} %</p>
     <v-chip>
-      {{  saving}}
+      {{ saving }}
     </v-chip>
     <v-chip>
-          {{  everyone}}
+      {{ everyone }}
     </v-chip>
     <v-chip>
       {{ remote }}
     </v-chip>
-<v-divider :thickness="1"></v-divider>
-
-    
-
+    <v-divider :thickness="1"></v-divider>
   </div>
 </template>
 
@@ -28,77 +29,60 @@ import { ref } from "vue";
 import { useRouter } from "vue-router";
 import { onMounted } from "vue";
 
-
-const item = ref(null)
+const item = ref(null);
 const props = defineProps({
-  savingProduct: Object
-
-})// 최고 저축 금리
-const maxRate1 = ref(null)
+  savingProduct: Object,
+}); // 최고 저축 금리
+const maxRate1 = ref(null);
 // 최고 최고우대 금리
-const maxRate2 = ref(null)
-const maxrateitme = ref(null)
+const maxRate2 = ref(null);
+const maxrateitme = ref(null);
 
-const everyone = ref('')
-const remote = ref('직접가입')
-const saving = ref('')
+const everyone = ref("");
+const remote = ref("직접가입");
+const saving = ref("");
 
-
-onMounted(() => {
-  axios({
-    method: 'get',
-    url: `http://127.0.0.1:8000/api/deposits/saving-product/${props.savingProduct.fin_prdt_cd}`
-  })
-  .then((res) => {
-    item.value = res.data
-  })
-  .then((res) => {
-    maxRate1.value = item.value.reduce((max, currentValue) => {
+maxRate1.value = props.savingProduct.savingoptions_set.reduce(
+  (max, currentValue) => {
     if (currentValue.intr_rate > max) {
-      return currentValue.intr_rate; 
+      return currentValue.intr_rate;
     }
-    return max; 
-    }, 0);
-    maxRate2.value = item.value.reduce((max, currentValue) => {
+    return max;
+  },
+  0
+);
+maxRate2.value = props.savingProduct.savingoptions_set.reduce(
+  (max, currentValue) => {
     if (currentValue.intr_rate2 > max) {
-      maxrateitme.value = currentValue
-      return currentValue.intr_rate2; 
+      maxrateitme.value = currentValue;
+      return currentValue.intr_rate2;
     }
-    return max; 
-    }, 0); 
-  })
-  .then((res) => {
-    if (props.savingProduct.join_deny === 1) {
-      everyone.value = '누구나가입'
-    } else if (props.savingProduct.join_deny === 3) {
-      everyone.value = '일부제한'
-    }
-    if (props.savingProduct.join_way !== undefined && (props.savingProduct.join_way.includes('인터넷') || (props.savingProduct.join_way.includes('스마트폰'))) ) {
-      remote.value = '방문없이가입'
-    }
-    if (maxrateitme.value.rsrv_type_nm === '자유적립식') {
-      saving.value = '자유적금'
-    } else {
-      saving.value = '정기적금'
-    }
-    
-    
-  })
-})
+    return max;
+  },
+  0
+);
 
-
-
-
-
-
-
-
-
+if (props.savingProduct.join_deny === 1) {
+  everyone.value = "누구나가입";
+} else if (props.savingProduct.join_deny === 3) {
+  everyone.value = "일부제한";
+}
+if (
+  props.savingProduct.join_way !== undefined &&
+  (props.savingProduct.join_way.includes("인터넷") ||
+    props.savingProduct.join_way.includes("스마트폰"))
+) {
+  remote.value = "방문없이가입";
+}
+if (maxrateitme.value.rsrv_type_nm === "자유적립식") {
+  saving.value = "자유적금";
+} else {
+  saving.value = "정기적금";
+}
 </script>
 
 <style scoped>
 .icon {
   box-shadow: 1px 2px 8px #cccccc;
 }
-
 </style>
