c301929ce0076dce5b576eeaa7670ea9e3fc3955
HyunJik <hjl0122@gmail.com>
2023-11-23 01:11:52 +0900

    게시판 디자인

--- final-pjt-front/front/src/views/CartView.vue
+++ final-pjt-front/front/src/views/CartView.vue
@@ -1,83 +1,105 @@
 <template>
-    <!-- <div>
-        <h1>찜한 상품</h1>
-        <div v-if="cartItems" class="product-list">
-            <div v-for="product in cartItems" :key="product.id" class="product-card">
-                <img :src="product.image" alt="">
-                <strong>{{ product.title }}</strong>
-                <p>상품 설명 : ${{ product.content }}</p>
-                <button @click="goDetail(product)">상세페이지로 이동</button>
-                <button @click="removeCart(product)">찜한 상품 삭제</button>  
-            </div>
-        </div>
-        <div v-else>
-        <strong>찜한 상품이 없습니다.</strong>
-        </div>
-    </div> -->
-    <div>
-    <h2>Cart View</h2>
-    <hr>
-    <p class="font-weight-bold">예금!</p>
-    <div v-if="likedDepositProducts.length === 0">
-      <p>동록된 예금 상품이 없습니다.</p>
-    </div>
-    <div v-else>
-      <ul>
-        <li v-for="depositProduct in likedDepositProducts" :key="depositProduct.id">
-          {{ depositProduct.fin_prdt_nm }}
-        </li>
-      </ul>
-    </div>
+  <v-container>
+    <v-row class="mb-5">
+      <v-col>
+        <v-divider class="mx-auto mb-3"></v-divider>
+        <h2 class="text-h5 font-weight-bold">장바구니</h2>
+        <v-divider class="mx-auto mb-3"></v-divider>
+      </v-col>
+    </v-row>
 
-    <hr>
-    <hr>
-    <p class="font-weight-bold">적금!</p>
-    <div v-if="likedsavingProducts.length === 0">
-      <p>동록된 적금 상품이 없습니다.</p>
-    </div>
-    <div v-else>
-      <ul>
-        <li v-for="savingProduct in likedsavingProducts" :key="savingProduct.id">
-          {{ savingProduct.fin_prdt_nm }}
-        </li>
-      </ul>
-    </div>
-  </div>
+    <!-- 예금 상품 -->
+    <v-row>
+      <v-col>
+        <h3 class="text-h6 font-weight-bold">예금</h3>
+        <v-divider class="mx-auto mb-3"></v-divider>
+
+        <v-row v-if="likedDepositProducts.length === 0">
+          <v-col>
+            <p>등록된 예금 상품이 없습니다.</p>
+          </v-col>
+        </v-row>
+
+        <v-row v-else>
+          <v-col>
+            <v-list>
+              <v-list-item-group>
+                <v-list-item v-for="depositProduct in likedDepositProducts" :key="depositProduct.id">
+                  <v-list-item-content>
+                    {{ depositProduct.fin_prdt_nm }}
+                  </v-list-item-content>
+                  <v-list-item-action>
+                    <v-btn @click="removeCart(depositProduct)" height="18" style="border-radius: 5px; max-width: 50px;">삭제</v-btn>
+                  </v-list-item-action>
+                </v-list-item>
+              </v-list-item-group>
+            </v-list>
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-row>
+
+    <!-- 중앙 구분선 -->
+    <v-divider class="mx-auto my-3"></v-divider>
+
+    <!-- 적금 상품 -->
+    <v-row>
+      <v-col>
+        <h3 class="text-h6 font-weight-bold">적금</h3>
+        <v-divider class="mx-auto mb-3"></v-divider>
+
+        <v-row v-if="likedsavingProducts.length === 0">
+          <v-col>
+            <p>등록된 적금 상품이 없습니다.</p>
+          </v-col>
+        </v-row>
+
+        <v-row v-else>
+          <v-col>
+            <v-list>
+              <v-list-item-group>
+                <v-list-item v-for="savingProduct in likedsavingProducts" :key="savingProduct.id">
+                  <v-list-item-content>
+                    {{ savingProduct.fin_prdt_nm }}
+                  </v-list-item-content>
+                  <v-list-item-action>
+                    <v-btn @click="removeCart(savingProduct)" height="18" style="border-radius: 5px; max-width: 50px;">삭제</v-btn>
+                  </v-list-item-action>
+                </v-list-item>
+              </v-list-item-group>
+            </v-list>
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script setup>
 import { ref } from 'vue'
 import { useRouter } from 'vue-router'
 
+const router = useRouter()
+
 const likedDepositProducts = JSON.parse(localStorage.getItem('likedDepositProducts')) || [];
 const likedsavingProducts = JSON.parse(localStorage.getItem('likedsavingProducts')) || [];
 
+const removeCart = (product) => {
+  const likedDepositProductsIndex = likedDepositProducts.findIndex((item) => item.id === product.id);
+  const likedsavingProductsIndex = likedsavingProducts.findIndex((item) => item.id === product.id);
 
-// const router = useRouter()
+  if (likedDepositProductsIndex !== -1) {
+    likedDepositProducts.splice(likedDepositProductsIndex, 1);
+    localStorage.setItem('likedDepositProducts', JSON.stringify(likedDepositProducts));
+  } else if (likedsavingProductsIndex !== -1) {
+    likedsavingProducts.splice(likedsavingProductsIndex, 1);
+    localStorage.setItem('likedsavingProducts', JSON.stringify(likedsavingProducts));
+  }
 
-// const cartItems = ref(null)
-
-// cartItems.value = JSON.parse(localStorage.getItem('cart'))
-
-// const goDetail = (product) => {
-//   router.push(`/${product.id}`)
-// }
-
-// const removeCart = (product) => {
-//   // localStoage 에서 삭제
-
-//   // 삭제할 아이템 index 검색
-//   const itemIdx = cartItems.value.findIndex((item) => item.id === product.id)
- 
-//   // 데이터 삭제
-//   cartItems.value.splice(itemIdx, 1)
-
-//   // 삭제된 데이터를 기준으로 데이터를 반영
-//   localStorage.setItem('cart', JSON.stringify(cartItems.value))
-// }
-
+  router.go()
+}
 </script>
 
 <style scoped>
-
+/* 필요한 경우에 스타일을 추가하세요. */
 </style>
